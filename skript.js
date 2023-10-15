a6674066f0133c8bbca1caecf4a307ef5dd05a82
an03er <144229434+an03er@users.noreply.github.com>
2023-10-15 20:25:52 +0300

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,20 +1,12 @@
-function openPopup() {
-  var popup = document.querySelector('.popup');
-  popup.style.display = 'block';
-}
-
-function closePopup() {
-    var popup = document.querySelector('.popup');
-    popup.style.display = 'none';
-}
-
 function calc() {
     var type_flowers = document.getElementById("type_flowers");
-    var count = document.getElementById("count");
-    var result = document.getElementById("result"); 
-
-    var price = 0;
-    price += parseInt(type_flowers.options[type_flowers.selectedIndex].value);
-    price = parseInt(count.value) * price;
-    result.innerHTML = price;
+    var count = document.getElementById("count").value;
+    if(count==""){alert("Вы не указали количество");}
+    else if(count<0){alert("Количество не может быть отрицательным");}
+    else{
+      var price = 0;
+      price += parseInt(type_flowers.options[type_flowers.selectedIndex].value);
+      price = parseInt(count) * price;
+      document.getElementById('result').innerHTML = "Стоимость равна: "+ price +" р.";
+    }
 }
